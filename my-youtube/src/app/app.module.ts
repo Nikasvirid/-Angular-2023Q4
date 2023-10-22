e706efa6c42c2827c8be7bd32a3fe4069ef777dd
Svirid Veronika <sviridka-vp@mail.ru>
2023-10-22 16:43:15 +0300

    create search-results component

--- my-youtube/src/app/app.module.ts
+++ my-youtube/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import AppComponent from './app.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchResultsComponent,
   ],
   imports: [
     BrowserModule,
