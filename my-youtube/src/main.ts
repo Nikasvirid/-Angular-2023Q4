82faaf18b4aa19e2568cdbb827ee69612f3c0ddd
Veronika <100194539+Nikasvirid@users.noreply.github.com>
2023-10-26 08:45:33 +0300

    Update main.ts

--- my-youtube/src/main.ts
+++ my-youtube/src/main.ts
@@ -1,13 +1,8 @@
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { provideRouter } from '@angular/router';
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import routeConfig from './app/routes';
 // import AppModule from './app/app.module';
 import AppComponent from './app/app.component';
-
-// platformBrowserDynamic()
-//   .bootstrapModule(AppModule)
-//   .catch((err) => console.error(err));
 
 bootstrapApplication(
   AppComponent,
