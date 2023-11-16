07216a65b1efaec9769778d84417606fa5adf017
Svirid Veronika <sviridka-vp@mail.ru>
2023-11-16 08:43:25 +0300

    add new branch,install ngrx

--- my-youtube/src/main.ts
+++ my-youtube/src/main.ts
@@ -2,13 +2,15 @@ import { provideRouter } from '@angular/router';
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import routeConfig from './app/routes';
 import  AppComponent  from './app/app.component';
+import { provideStore } from '@ngrx/store';
 
 bootstrapApplication(
   AppComponent,
   {
     providers: [
-      provideProtractorTestingSupport(),
-      provideRouter(routeConfig),
-    ],
+    provideProtractorTestingSupport(),
+    provideRouter(routeConfig),
+    provideStore()
+],
   },
 ).catch((err) => console.error(err));
